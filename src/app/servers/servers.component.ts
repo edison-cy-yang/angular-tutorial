45dda0e8738646a9f810e6df6689b594946e8a8b
edison-cy-yang <edison.cy.yang@gmail.com>
2020-05-04 11:57:36 -0700

    assignment to add input two way binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,6 +9,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'Test server';
+  username = ""
 
   constructor() {
     setTimeout(() => {
@@ -20,11 +21,15 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = "Server was created!";
+    this.serverCreationStatus = "Server was created, name is " + this.serverName;
   }
 
   onUpdateServerName(event: any) {
     this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  onResetUsername(event: Event) {
+    this.username = "";
   }
 
 }
